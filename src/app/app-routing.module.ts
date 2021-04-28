e637da7ed71e80797810c38e021f67c83ccce699
kaokei <kaokei.rst@gmail.com>
2021-04-28 23:07:49 +0800

    增加about页面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './pages/about/about.component';
+
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
